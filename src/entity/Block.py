71ea2ab48914cf5d3ec28f19c6da79e1227865a5
徐可瑶 <19241018@bjtu.edu.cn>
2024-05-08 11:08:32 +0800

    update

---
+++ src/entity/Block.py
@@ -0,0 +1,7 @@
+class Block:
+    def __init__(self, idx=None, sku=None):
+        self.idx = idx # 订单编号
+        if sku is None: # 订单包括的sku列表
+            self.sku = []
+        else:
+            self.sku = sku
